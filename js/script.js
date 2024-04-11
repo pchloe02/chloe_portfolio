1cc587849d66d039ba3ab5cea1999031c3e32ce8
PELERIN Chloé <pelerinchloe@gmail.com>
2024-04-12 01:15:09 +0200

    initialize project

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// gsap.registerPlugin(ScrollTrigger);
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     gsap.to('.title', {
+    
+//         fontSize:'1.5rem',
+//         top: "3rem",
+//         left: "11%",
+//         scrollTrigger: {
+    
+//             trigger:'.title',
+//             start:'top top',
+//             end: 1000,
+//             scrub: 2
+//         }
+//     })
+
+//     gsap.to('.color-block', {
+    
+//         height: '100px',
+//         width: '15%',
+//         left: '3.5%',
+//         opacity: 0,
+
+//         scrollTrigger: {
+    
+//             trigger:'.title',
+//             start:'top top',
+//             end: 1000,
+//             scrub: 2
+//         }
+//     })
+
+// })
