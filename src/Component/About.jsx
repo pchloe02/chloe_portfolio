a13594da2fc913ff3e71631c5c912b2a4ba44c0c
PELERIN Chloé <pelerinchloe@gmail.com>
2024-04-21 01:11:44 +0200

    mise en page de About

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './About.css'
+import profil from "../assets/img/reading-book.png"
 
 function About() {
   return (
-    <div className='about'>
-        <h1>A propos de moi </h1>
+    <div className='about-container'>
+        <div className='about'>
+            <div>
+                <img  id="pp" src={profil} alt="picture-profil"/>
+            </div>
+
+            <div>
+                <h2>Qui suis-je ?</h2>
+                <p id="about-me">
+                <span id="bold">Je suis une apprenante chez Ada Tech School à la 
+                recherche d’une alternance en développement front-end.</span>
+                <br/>
+                <br/>
+                Après 2 ans passés à étudier les arts plastiques à la fac, 
+                j’ai décidé de troquer mes toiles contre des lignes de code. 
+                Attirée par l’alliance entre la créativité et la technologie,
+                j’ai choisi de me spécialiser dans le développement web.
+            </p>
+            </div>
+        </div>
     </div>
   )
 }
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../assets/cat.png'
+import logo from '../assets/img/cat.png'
 
 
 function Navbar() {
