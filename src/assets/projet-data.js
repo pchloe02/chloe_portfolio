7b1893ff94557786432b384eb66c8410671bff54
pchloe02 <pelerinchloe@gmail.com>
2026-04-14 23:19:35 +0200

    fix - update project

--- src/assets/projet-data.js
+++ src/assets/projet-data.js
@@ -1,50 +1,50 @@
 import daily_space from '../assets/img/project1_img.jpg'
 import on_meuble from '../assets/img/on_meuble.png'
-import art_imprint from '../assets/img/login_page.png'
+import oven_tales from '../assets/img/oventales.png'
 import loop from '../assets/img/Loop.png'
 
 
 const projetData =
-[
-    {
-        "name": "The Daily Space",
-        "type": "Projet collectif",
-        "description": "Un site pour les fans de l'Espace, découvrez chaque jour une image officiel de la Nasa.",
-        "apercu": daily_space ,
-        "github": "https://github.com/pchloe02/projet-collectif-dataviz-lisa-chloe-eliana-julien",
-        "demo": "lien demo",
-        "techno": "Html & CSS, JavaScript"
+    [
+        {
+            "name": "The Daily Space",
+            "type": "Projet collectif",
+            "description": "Un site pour les fans de l'Espace, découvrez chaque jour une image officiel de la Nasa.",
+            "apercu": daily_space,
+            "github": "https://github.com/pchloe02/projet-collectif-dataviz-lisa-chloe-eliana-julien",
+            "demo": "lien demo",
 
 
-    },
-    {
-        "name": "On Meuble",
-        "type": "Projet collectif",
-        "description": "Ce projet est un site de vente de meubles pour l'entreprise Anciens meubles pour une nouvelle vie destiné à Lauréline Fleury",
-        "apercu": on_meuble,
-        "github": "https://github.com/adatechschool/onmeuble",
-        "demo": "lien demo",
-        "techno": "React, CSS, NodeJs & Express, Supabase"
 
-    },
-    {
-        "name": "Art Imprint",
-        "type": "Projet personnel (en cours)",
-        "description": "Une plateforme dédiée aux artistes permettant de créer un suivi et de visualiser son évolution au fil du temps.",
-        "apercu": art_imprint,
-        "github": "https://github.com/pchloe02/art-imprint",
-        "demo": "lien demo",
-        "techno": "React, CSS (TBD)"
-    },
-    {
-        "name": "Loop",
-        "type": "Projet collectif (en cours)",
-        "description": "Loop est une application qui permet de faciliter la découverte de jolis lieux urbains inconnus autour de soi.",
-        "apercu": loop,
-        "github": "https://github.com/adatechschool/loop-app",
-        "demo": "lien demo",
-        "techno": "Front : React, Typecript | Back : NodeJs/Express | Db : PostgreSQL"
-    }
-];
+        },
+        {
+            "name": "On Meuble",
+            "type": "Projet collectif",
+            "description": "Ce projet est un site de vente de meubles pour l'entreprise Anciens meubles pour une nouvelle vie destiné à Lauréline Fleury",
+            "apercu": on_meuble,
+            "github": "https://github.com/adatechschool/onmeuble",
+            "demo": "lien demo",
 
-export {projetData}
+
+        },
+        {
+            "name": "Oven Tales",
+            "type": "Projet personnel (en cours)",
+            "description": "Oven Tales est un blog communautaire dédiée au partage de recettes de cuisine. Créez et partagez vos recettes. Commentez et interagissez avec la communauté !",
+            "apercu": oven_tales,
+            "github": "https://github.com/pchloe02/oven-tales-blog",
+            "demo": "lien demo",
+
+        },
+        {
+            "name": "Loop",
+            "type": "Projet collectif (en cours)",
+            "description": "Loop est une application qui permet de faciliter la découverte de jolis lieux urbains inconnus autour de soi.",
+            "apercu": loop,
+            "github": "https://github.com/adatechschool/loop-app",
+            "demo": "lien demo",
+
+        }
+    ];
+
+export { projetData }
